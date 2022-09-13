5725372ceea60d5613bc2aa7f8a9516ad03b4e13
Roman Osypchuk <akaShalffei@gmail.com>
2022-09-13 19:49:38 +0200

    Added TimofeyModel (Second variant (c) Tiloran)

--- ShopLogic/Models/TimofeyModel.cs
+++ ShopLogic/Models/TimofeyModel.cs
@@ -5,4 +5,28 @@
         public int PopularProductId { get; set; }
         public List<User> Users { get; set; } = new List<User>();
     }
+    public class TimofeyModel2
+    {
+        public int PopularProductId { get; set; }
+        public List<TimofeyModelUserData> Users { get; set; }
+    }
+
+    public class TimofeyModelUserData
+    {
+        public int UserId { get; set; }
+        public string UserName { get; set; }
+        public List<TimofeyModelProductData> ProductData { get; set; }
+        public decimal TotalSum { get; set; }
+    }
+
+    public class TimofeyModelProductData
+    {
+        public int ProductId { get; set; }
+        public int ProductCount { get; set; }
+        public string ProductName { get; set; }
+        public decimal Price { get; set; }
+        public decimal Sum { get; set; }
+    }
 }
+
+
--- ShopLogic/Servise/LocalDbServiseOrder.cs
+++ ShopLogic/Servise/LocalDbServiseOrder.cs
@@ -68,19 +68,41 @@ namespace ShopLogic.Servise
             }
             return "Selected orders have been deleted";
         }
-        public TimofeyModel GetDateOrdersWithUser (ApplicationContext db, DateTime start, DateTime finish)
+        public TimofeyModel2 GetDateOrdersWithUser (ApplicationContext db, DateTime start, DateTime finish)
         {
-            
-            TimofeyModel timofeyModel = new TimofeyModel();
-            timofeyModel.Users = db.Users.Include(x => x.UserOrders.Where(x => x.Created >= start && x.Created <= finish)).ToList();
-            timofeyModel.PopularProductId = db.Products.Include(x => x.ProductOrders.Where(x => x.Created >= start && x.Created <= finish)).OrderByDescending(x => x.BuyCount).Select(x => x.Id).FirstOrDefault();
-            foreach (var user in timofeyModel.Users)
+            TimofeyModel2 timofeyModel2 = new TimofeyModel2 { Users = new List<TimofeyModelUserData>() }; 
+            timofeyModel2.PopularProductId = db.Products.Include(x => x.ProductOrders.Where(x => x.Created >= start && x.Created <= finish)).OrderByDescending(x => x.BuyCount).Select(x => x.Id).FirstOrDefault();
+            var usersId = db.Users.Include(user => user.UserOrders.Where(order => order.Created >= start && order.Created <= finish)).Select(user => user.Id).ToList();
+            for (int i = 0; i < usersId.Count; i++)
             {
-                user.AllCountOrders = user.UserOrders.Count;
-                user.TotalAmount = user.UserOrders.Sum(x => x.Price);
-            }
-            Console.ReadLine();
-            return timofeyModel;
+                TimofeyModelUserData user = new TimofeyModelUserData { ProductData = new List<TimofeyModelProductData>() };
+                user.UserId = usersId[i];
+                user.UserName = db.Users.Where(user => user.Id == usersId[i]).Select(user => user.Name).Single();
+                user.TotalSum = db.Orders.Where(order => order.Created >= start && order.Created <= finish && order.UserId == usersId[i]).Sum(order => order.Price);
+                var productId = db.Orders.Where(order => order.IsPayed == true && order.Created >= start && order.Created <= finish && order.UserId == usersId[i]).Select(x => x.Product.Id).ToList();
+                for (int j = 0; j < productId.Count; j++)
+                {
+                    TimofeyModelProductData product = new TimofeyModelProductData();
+                    product.ProductId = productId[j];
+                    product.ProductName = db.Products.Where(prod => prod.Id == productId[j]).Select(prod => prod.Name).Single();
+                    product.Price = db.Products.Where(prod => prod.Id == productId[j]).Select(prod => prod.Price).FirstOrDefault();
+                    product.ProductCount = db.Orders.Where(order => order.ProductId == productId[j] && order.UserId == usersId[i] && order.Created >= start && order.Created <= finish).Count();
+                    product.Sum = db.Orders.Where(order => order.ProductId == productId[j] && order.UserId == usersId[i] && order.Created >= start && order.Created <= finish).Sum(x => x.Price);
+                    user.ProductData.Add(product);
+                }
+                timofeyModel2.Users.Add(user);
+            }     
+
+
+            //TimofeyModel timofeyModel = new TimofeyModel();
+            //timofeyModel.Users = db.Users.Include(x => x.UserOrders.Where(x => x.Created >= start && x.Created <= finish)).ToList();
+            //timofeyModel.PopularProductId = db.Products.Include(x => x.ProductOrders.Where(x => x.Created >= start && x.Created <= finish)).OrderByDescending(x => x.BuyCount).Select(x => x.Id).FirstOrDefault();
+            //foreach (var user in timofeyModel.Users)
+            //{
+            //    user.AllCountOrders = user.UserOrders.Count;
+            //    user.TotalAmount = user.UserOrders.Sum(x => x.Price);
+            //}
+            return timofeyModel2;
         }
     }
 }
