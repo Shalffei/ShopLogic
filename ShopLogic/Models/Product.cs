7fde3e7da48dd751e3522ca3bc52652a56152cf4
Roman Osypchuk <akaShalffei@gmail.com>
2022-09-15 10:22:37 +0200

    Added ProductCategory

--- ShopLogic/Models/Product.cs
+++ ShopLogic/Models/Product.cs
@@ -3,6 +3,7 @@
     public class Product
     {
         public int Id { get; set; }
+        public int ProductCategoryId { get; set; }
         public string Name { get; set; }
         public decimal Price { get; set; }
         public int? BuyCount { get; set; }
